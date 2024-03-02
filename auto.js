c6efb07e0f19df6ae9773df3f1f786a8df6e418f
qingkaishi <qingkaishi@gmail.com>
2024-03-02 07:50:41 -0500

    auto update

--- auto.js
+++ auto.js
@@ -6,16 +6,16 @@ var browser = {
             trident: u.indexOf('Trident') > -1, //IE内核
             presto: u.indexOf('Presto') > -1, //opera内核
             webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') === -1, //火狐内核
             mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
             ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
             android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
             iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
             iPad: u.indexOf('iPad') > -1, //是否iPad
-            webApp: u.indexOf('Safari') == -1,//是否web应该程序，没有头部与底部
-            wechat: ua.match(/MicroMessenger/i) == "micromessenger",//微信
-            weibo: ua.match(/WeiBo/i) == "weibo",//微博
-            qq: ua.match(/QQ/i) == "qq"//qq
+            webApp: u.indexOf('Safari') === -1,//是否web应该程序，没有头部与底部
+            wechat: ua.match(/MicroMessenger/i) === "micromessenger",//微信
+            weibo: ua.match(/WeiBo/i) === "weibo",//微博
+            qq: ua.match(/QQ/i) === "qq"//qq
         };
     }(),
     language: (navigator.browserLanguage || navigator.language).toLowerCase()
